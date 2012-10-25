f0832c208528c9a31efa4038a12e018a98232816
Kyle Johnson <kjohnson@gnulnx.net>
2012-10-25 10:23:24 -0400

    More cleaning up

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,13 +1,14 @@
 # Manage the rtorrent service
-# This will ensure that the rtorrent service is running and is enabled on startup.
+# This will ensure that the rtorrent service is running and is enabled on
+# startup.
 class rtorrent::service {
 
   service {'rtorrent':
-    hasstatus	=> true,
-    hasrestart	=> true,
-    enable	=> true,
-    ensure	=> running,
-    require	=> Class['rtorrent::config']
+    ensure      => running,
+    hasstatus   => true,
+    hasrestart  => true,
+    enable      => true,
+    require     => Class['rtorrent::config']
   }
 
 }
