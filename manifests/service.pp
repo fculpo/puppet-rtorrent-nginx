d9ea64a624ad61a1670238537c9f8a582f8b87e5
Kyle Johnson <kjohnson@gnulnx.net>
2012-10-25 10:13:06 -0400

    Cleaned up the code per puppet lint

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,10 +1,13 @@
+# Manage the rtorrent service
+# This will ensure that the rtorrent service is running and is enabled on startup.
 class rtorrent::service {
 
-  service {"rtorrent":
-    hasstatus => true,
-    hasrestart => true,
-    enable => true,
-    require => Class["rtorrent::config"]
+  service {'rtorrent':
+    hasstatus	=> true,
+    hasrestart	=> true,
+    enable	=> true,
+    ensure	=> running,
+    require	=> Class['rtorrent::config']
   }
 
 }
