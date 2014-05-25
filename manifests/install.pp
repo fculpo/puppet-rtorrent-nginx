443ee9514c9f256683f27023591187e5daf27145
Leon Blakey <leon.m.blakey@gmail.com>
2014-05-25 10:41:52 -0400

    Fixed broken root and fastcgi config

--- manifests/install.pp
+++ manifests/install.pp
@@ -44,10 +44,12 @@ class rtorrent::install {
 	nginx::resource::vhost { "quacknas-remote":
 		ensure                => present,
 		listen_port           => 80,
-		www_root              => "/var/www",
+		#www_root             => "/var/www", #broken, incorectly puts root in location /{} block
+		vhost_cfg_prepend	  => {root =>"/var/www"},
 		proxy                 => undef,
 		location_cfg_append   => undef,
 		index_files           => [ 'index.php', 'index.html' ],
+		use_default_location   => false
 	}
 	file { "/etc/nginx/conf.d/default.conf":
 		ensure => purged
@@ -64,8 +66,8 @@ class rtorrent::install {
 		location        => '~ \.php$',
 		index_files     => ['index.php', 'index.html', 'index.htm'],
 		proxy           => undef,
-		fastcgi         => "unix:/var/run/php5-fpm.sock",
-		fastcgi_script  => undef,
+		fastcgi         => 'unix:/var/run/php5-fpm.sock',
+		fastcgi_script  => '$document_root$fastcgi_script_name',  
 		location_cfg_append => {
 			fastcgi_connect_timeout => '3m',
 			fastcgi_read_timeout    => '3m',
