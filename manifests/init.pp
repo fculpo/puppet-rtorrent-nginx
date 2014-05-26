342830fecc13762a85c08de7f15d91b54822cb53
Leon Blakey <leon.m.blakey@gmail.com>
2014-05-26 14:55:35 -0400

    Use class definitons instead of include due to parameters in init

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,10 +3,16 @@ class rtorrent(
 	$rtorrent_upload_rate = 20,
 	$rtorrent_max_uploads = 8,
 	$rtorrent_session = '/quackdrive/torrents/rtsession',
-	$rtorrent_directory = '/quackdrive/torrents'
+	$rtorrent_directory = '/quackdrive/torrents',
+	$www_dir = '/var/www',
+	$rutorrent_dir = 'rutorrent'
 ) {
-	
-	include rtorrent::config
-	include rtorrent::service
-	include rtorrent::install
+	class { 'rtorrent::nginx_php': 
+		www_dir => $www_dir,
+	}
+	class { 'rtorrent::rtorrent_build': }
+	class { 'rtorrent::rtorrent_config': }
+	class { 'rtorrent::rutorrent': 
+		rutorrent_fulldir => "$www_dir/$rutorrent_dir"
+	}
 }
