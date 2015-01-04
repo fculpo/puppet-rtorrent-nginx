ed8bbcb9718b3ad72ee9241fdefbcbbe852d0f91
Leon Blakey <lord.quackstar@gmail.com>
2015-01-04 02:02:03 -0500

    Renamed default .rtorrent.rc to .rtorrent.rc.puppet

--- manifests/rtorrent_config.pp
+++ manifests/rtorrent_config.pp
@@ -28,7 +28,7 @@ class rtorrent::rtorrent_config(
   }
   
   # Create .rtorrent.rc
-  file { '/home/rtorrent/.rtorrent.rc.main':
+  file { '/home/rtorrent/.rtorrent.rc.puppet':
     ensure  => present,
     owner   => 'rtorrent',
     group   => 'rtorrent',
